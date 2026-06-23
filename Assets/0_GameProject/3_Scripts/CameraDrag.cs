c0779b2b083e9af103092435bf8785aa9d5b2504
kienvu15 <88vutrunkien@gmail.com>
2026-06-23 21:46:35 +0700

    menu UI

---
+++ Assets/0_GameProject/3_Scripts/CameraDrag.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+    private Camera cam;
+    private Plane groundPlane;
+
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+        groundPlane = new Plane(Vector3.up, Vector3.zero);
+    }
+
+    void Update()
+    {
+        if (cam == null) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (groundPlane.Raycast(ray, out float entry))
+            {
+                dragOrigin = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (groundPlane.Raycast(ray, out float entry))
+            {
+                Vector3 currentPoint = ray.GetPoint(entry);
+                Vector3 difference = dragOrigin - currentPoint;
+                transform.position += difference;
+            }
+        }
+    }
+}
